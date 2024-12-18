202c00ef6e79e20e82b3bc5fa7844de18d6e707c
Andrewxyz123 <drakenlabo@gmail.com>
2024-12-18 12:48:16 +0700

    First Setup Project

---
+++ backend/meet-easy/src/main/java/com/dna/meet_easy/MeetEasyApplication.java
@@ -0,0 +1,13 @@
+package com.dna.meet_easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetEasyApplication.class, args);
+	}
+
+}
